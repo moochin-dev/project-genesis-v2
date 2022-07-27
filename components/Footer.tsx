0ffdb1332205933c591f77e2a3e0fcbad66e378e
obzva <dngyng1000@gmail.com>
2022-07-27 21:26:54 +0900

    tailwind css configuration setting

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div style={{ textAlign: "center" }}>
+        <h1>Footer</h1>
+      </div>
+    </footer>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div style={{ textAlign: "center" }}>
+        <h1>Navbar</h1>
+      </div>
+    </nav>
+  );
+}
